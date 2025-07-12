564d0b358504acd38dc535d684aea3c0d001ad4f
shemishtamesh <shemishtamail@gmail.com>
2025-07-12 20:28:14 +0300

    adding a package to the flake

--- flake.nix
+++ flake.nix
@@ -7,12 +7,20 @@
   outputs =
     { nixpkgs, ... }:
     let
+      pkgs = system: nixpkgs.legacyPackages.${system};
       for_all_systems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.doubles.all;
     in
     {
       devShells = for_all_systems (system: {
-        default = nixpkgs.legacyPackages.${system}.mkShell {
-          packages = with nixpkgs.legacyPackages.${system}; [ cargo ];
+        default = (pkgs system).mkShell {
+          packages = with (pkgs system); [ cargo ];
+        };
+      });
+      packages = for_all_systems (system: {
+        default = (pkgs system).rustPlatform.buildRustPackage {
+          name = "termtris";
+          src = ./.;
+          cargoHash = "sha256-ZeKC0QGwn+wCel/mtSxldjVSWGbuoduJ5Crq56zXpw0=";
         };
       });
     };
